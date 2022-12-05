cd4bde6a21477b9924fecf37427b91e28da558af
Victor Tang <victor.tang@adyen.com>
2022-12-05 16:47:28 +0800

    fix cors

--- server.js
+++ server.js
@@ -9,22 +9,8 @@ const checkout = require('./routes/api/checkout');
 // const frontEnd = require('./routes/front_end/front_end');
 
 const app = express();
-app.use(function(req, res, next) {
-      // res.header("Access-Control-Allow-Origin", "*");
-      const allowedOrigins = ['http://localhost:5000', 'https://adyen-integration-playground.onrender.com/'];
-      const origin = req.headers.origin;
-      if (allowedOrigins.includes(origin)) {
-           res.setHeader('Access-Control-Allow-Origin', origin);
-      }
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-      res.header("Access-Control-Allow-credentials", true);
-      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-      next();
-    });
-
+app.use(cors());
 app.use(bodyParser.json());
-
-
 
 // app.use('/images', express.static(__dirname + '/images'));
 // app.use(express.static(__dirname + '/public'));
